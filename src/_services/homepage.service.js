5d2608004cbec6e6584626f0987f89d9cacaff43
aeolus87 <callejo.julius.07222003@gmail.com>
2024-10-29 16:47:12 +0800

    added render api on service

--- src/_services/homepage.service.js
+++ src/_services/homepage.service.js
@@ -1,13 +1,14 @@
 // _services/homepage.service.js
 import axios from "axios";
 
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3002";
 export const homepageService = {
   getHomepageData,
 };
 
 function getHomepageData() {
   return axios
-    .get("http://localhost:3002/api/homepage")
+    .get(`${API_URL}/api/homepage`)
     .then((response) => {
       const data = response.data[0] || response.data;
       return {
